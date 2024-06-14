93a9213418d0e3a709b488da96368cb717eee0c0
Ezekiel <bussyyoni400@gmail.com>
2024-06-14 13:25:28 +0100

    First commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_HOST = "https://brightjohn.pythonanywhere.com/" if not DEBUG else ""
+STATIC_HOST = "https://brightjohn.pythonanywhere.com/"
 STATIC_URL = STATIC_HOST + "/static/"
 MEDIA_URL = "/media/"
 
@@ -131,14 +131,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
